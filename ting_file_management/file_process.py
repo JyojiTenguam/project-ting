97c11265e324500b86d8781b9a412dfb53f0e328
JyojiTenguam <j.y.oji@hotmail.com>
2024-10-09 18:17:37 -0300

    Resolvendo requisito 03 de Project ting

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance._data:
+        if item["nome_do_arquivo"] == path_file:
+            return
+
+    lines = txt_importer(path_file)
+
+    if lines is not None:
+        file_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+
+        instance.enqueue(file_data)
+        print(file_data)
 
 
 def remove(instance):
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,20 +3,23 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self._items = []
+        self._data = []
 
     def enqueue(self, item):
-        self._items.append(item)
+        self._data.append(item)
 
     def dequeue(self):
-        if len(self._items) == 0:
+        if len(self._data) == 0:
             raise IndexError("Índice Inválido ou Inexistente")
-        return self._items.pop(0)
+        return self._data.pop(0)
 
     def search(self, index):
-        if index < 0 or index >= len(self._items):
+        if index < 0 or index >= len(self._data):
             raise IndexError("Índice Inválido ou Inexistente")
-        return self._items[index]
+        return self._data[index]
 
     def __len__(self):
-        return len(self._items)
+        return len(self._data)
+
+    def is_empty(self):
+        return self._data == []
