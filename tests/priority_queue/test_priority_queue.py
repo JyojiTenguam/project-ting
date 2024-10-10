7e477c94cd15d418415f9c07cd967150b73f293c
JyojiTenguam <j.y.oji@hotmail.com>
2024-10-09 21:32:46 -0300

    fix:Linter do requisito 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,12 +1,14 @@
 import pytest
 from ting_file_management.priority_queue import PriorityQueue
 
+
 def create_mock(file_name: str, lines: int):
     return {
         "nome_do_arquivo": file_name,
         "qtd_linhas": lines,
         "linhas_do_arquivo": [file_name] * lines,
     }
+
 
 def test_basic_priority_queueing():
     queue = PriorityQueue()
@@ -20,11 +22,11 @@ def test_basic_priority_queueing():
     ]
 
     ordered_items = [
-        "texto_01.txt",  # 1 linha
-        "texto_03.txt",  # 2 linhas
-        "texto_04.txt",  # 3 linhas
-        "texto_02.txt",  # 6 linhas
-        "texto_05.txt",  # 8 linhas
+        "texto_04.txt",  # 1 linha
+        "texto_01.txt",  # 2 linhas
+        "texto_03.txt",  # 3 linhas
+        "texto_05.txt",  # 6 linhas
+        "texto_02.txt",  # 8 linhas
     ]
 
     for item in items:
@@ -43,11 +45,11 @@ def test_basic_priority_queueing():
     assert len(queue) == 6
     assert queue.search(5)["nome_do_arquivo"] == "texto_06.txt"
 
-    # Remove o elemento com maior prioridade (texto_01.txt)
+    # Remove o elemento com maior prioridade (texto_04.txt)
     removed_element = queue.dequeue()
 
     assert removed_element is not None
-    assert removed_element["nome_do_arquivo"] == "texto_01.txt"
+    assert removed_element["nome_do_arquivo"] == "texto_04.txt"
     assert len(queue) == 5
 
     # Insere um novo arquivo com prioridade alta e verifica sua posição
@@ -56,15 +58,17 @@ def test_basic_priority_queueing():
 
     # Atualizar a ordem esperada após a inserção do novo elemento
     expected_order_after_enqueue = [
-        "texto_03.txt",  # 2 linhas
-        "texto_04.txt",  # 3 linhas
-        "texto_07.txt",  # 6 linhas
-        "texto_02.txt",  # 7 linhas
-        "texto_05.txt",  # 8 linhas
+        "texto_01.txt",  # 2 linhas
+        "texto_07.txt",  # 2 linhas
+        "texto_03.txt",  # 3 linhas
+        "texto_05.txt",  # 6 linhas
+        "texto_06.txt",  # 7 linhas
+        "texto_02.txt",  # 8 linhas
     ]
 
     for i in range(len(expected_order_after_enqueue)):
-        assert queue.search(i)["nome_do_arquivo"] == expected_order_after_enqueue[i]
+        assert queue.search(i)
+        ["nome_do_arquivo"] == expected_order_after_enqueue[i]
 
     # Verifica a exceção de índice inválido
     with pytest.raises(IndexError):
